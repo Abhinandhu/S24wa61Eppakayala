dba34426a23cdf41fe53f12d4c5210092102c160
Abhinandhu <36483329+Abhinandhu@users.noreply.github.com>
2024-03-20 12:02:37 -0500

    updated bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const num1 = Math.random();
-const num2 = Math.random();
+var num1 = Math.random();
+var num2 = Math.random();
 router.get('/', function(req, res) {
     const difference = Math.abs(num1 - num2);
     res.send(`The function Math.abs with numbes ${num1} and ${num2} returns ${difference}`);
