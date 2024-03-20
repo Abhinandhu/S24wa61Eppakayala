8f723447e8edd1feeca4266afbb42c90aa40d975
Abhinandhu <36483329+Abhinandhu@users.noreply.github.com>
2024-03-20 16:01:09 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
-var num1 = Math.random();
-var num2 = Math.random();
+
 router.get('/', function(req, res) {
-    const difference = Math.abs(num1 - num2);
-    res.send(`The function Math.abs with numbes ${num1} and ${num2} returns ${difference}`);
-});
-router.get('/', function(req, res) {
-    res.send('Respond with a resource');
+    var num1 = Math.random()+1; //just to make sure num1 value is greater then 1
+    var num2 = Math.random()+1; //just to make sure num2 value is greater then 1
+    const absvalue = Math.abs(num1 - num2);
+    
+    num1 = Math.min(Math.max(num1, -1), 1);
+    const acosresult = Math.acos(num1);
+
+    const sinresult = Math.sin(num1)
+    res.send(`Math.abs(num1 - num2) applied to ${num1} and ${num2} is ${absvalue}<br>
+    Math.acos(num1) applied to ${num1} is ${acosresult} <br>
+    Math.sin(num1) applied to ${num1} is ${sinresult} <br>
+    `);
+
 });
 
 module.exports = router;
