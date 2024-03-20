29f2c53c42ebe58e7941480a65c9991a699cc88d
Abhinandhu <36483329+Abhinandhu@users.noreply.github.com>
2024-03-20 16:09:13 -0500

    bonus updated

--- routes/computation.js
+++ routes/computation.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    var num1 = Math.random()+1; //just to make sure num1 value is greater then 1
-    var num2 = Math.random()+1; //just to make sure num2 value is greater then 1
+
+    var num1 = Math.random(); 
+    var num2 = Math.random();
+
     const absvalue = Math.abs(num1 - num2);
     
     num1 = Math.min(Math.max(num1, -1), 1);
+    
     const acosresult = Math.acos(num1);
 
     const sinresult = Math.sin(num1)
